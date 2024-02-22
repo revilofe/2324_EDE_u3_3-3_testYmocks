1807d2f1dd8006da3f0a570f20a86fec3e6e4b7c
Edu <revilofe@gmail.com>
2024-02-22 08:59:32 +0100

    Base test

---
+++ src/test/kotlin/es/iesra/exchange/ExchangeServiceTest.kt
@@ -0,0 +1,12 @@
+package es.iesra.exchange
+
+import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.matchers.shouldBe
+
+class ExchangeServiceTest : DescribeSpec({
+
+    beforeTest {
+    }
+
+    describe("exchange") { }
+})
